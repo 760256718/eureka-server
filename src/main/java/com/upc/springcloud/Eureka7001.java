8bc34f6028cdd0a28ce46c1edb2419b115fcafb5
wky_glb <abc_glb@qq.com>
2020-04-06 21:55:25 +0800

    第一次修改

--- src/main/java/com/upc/springcloud/Eureka7001.java
+++ src/main/java/com/upc/springcloud/Eureka7001.java
@@ -10,4 +10,8 @@ public class Eureka7001 {
 	public static void main(String[] args) {
 		SpringApplication.run(Eureka7001.class, args);
 	}
+	
+	public void test1() {
+		System.out.println("这是第一次修改");
+	}
 }
